ac6e572fe26782f503de6ee0d1935431971d756b
huntercront <38252870+huntercront@users.noreply.github.com>
2020-10-16 13:29:47 +0300

    add webp

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 WebFontConfig = {
-	google: { families: ['Muli:400,600,800'] }
+    google: { families: ['Mulish:400,600,800'] }
 };
 
 (function(d) {
-	var wf = d.createElement('script'), s = d.scripts[0];
-	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-	wf.async = true;
-	s.parentNode.insertBefore(wf, s);
+    var wf = d.createElement('script'),
+        s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
 })(document);
+
+
+let tabSelects = document.querySelectorAll('.tailored-tab-select-inner')
+tabSelects.forEach(function(tabSelect) {
+    tabSelect.addEventListener('click', function() {
+        document.querySelector('.tailored-tab-select-inner.active').classList.remove('active')
+        this.classList.add('active');
+    })
+})
