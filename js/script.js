18cb9b95b6df8cbc632d0fff9448ef94defa4ef3
huntercront <38252870+huntercront@users.noreply.github.com>
2020-11-07 17:35:05 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 WebFontConfig = {
-    google: { families: ['Mulish:400,600,800'] }
+    google: { families: ['Manrope:400,600,800'] }
 };
 
 (function(d) {
@@ -10,14 +10,64 @@ WebFontConfig = {
     s.parentNode.insertBefore(wf, s);
 })(document);
 
+
+// var play = new autoPlayTabs();
 let tabSelects = document.querySelectorAll('.tailored-tab-select-inner')
 tabSelects.forEach(function(tabSelect) {
     tabSelect.addEventListener('click', function() {
-        let recentActiveTab = document.querySelector('.tailored-tab-select-inner.active')
-        let nowActive = this;
-        recentActiveTab.classList.remove('active')
-        document.querySelector('[data-tab="' + recentActiveTab.getAttribute('data-for-tab') + '"]').classList.remove('active');
-        nowActive.classList.add('active');
-        document.querySelector('[data-tab="' + nowActive.getAttribute('data-for-tab') + '"]').classList.add('active')
+        this.querySelector('.tab-progress-indicator').style.width = '0%';
+        tabSwap(this.getAttribute('data-for-tab'));
+        play.stopLoop();
     })
 })
+
+document.querySelector('.tailored-selects').addEventListener('mouseover', function() {
+    play.stopLoop();
+    console.log(play.stopLoop())
+});
+document.querySelector('.tailored-selects').addEventListener('mouseleave', function() {
+    play.runTimer(document.querySelector('.tailored-tab-select-inner.active').getAttribute('data-for-tab'), 5);
+});
+
+function Timer() {
+
+    var timeinterval;
+    this.runTimer = function autoPlayTabs(startIndex, time) {
+        var start = 0;
+        var time = Math.round(time * 10);
+        var activeTab = document.querySelector('[data-for-tab="' + startIndex + '"]');
+        var activeTabIndicator = activeTab.querySelector('.tab-progress-indicator');
+        timeinterval = setInterval(function() {
+            if (start > 100) {
+                clearInterval(timeinterval);
+                if (startIndex >= 4) {
+                    startIndex = 1;
+                    autoPlayTabs(startIndex, 5)
+                    tabSwap(1)
+                } else {
+                    startIndex++;
+                    tabSwap(startIndex)
+                    autoPlayTabs(startIndex, 5)
+                }
+            } else {
+                activeTabIndicator.style.width = start + '%';
+            }
+            start++;
+        }, time);
+
+        this.stopLoop = function stopLoopTabs() {
+            clearInterval(timeinterval);
+        }
+    }
+}
+
+function tabSwap(tabIndex) {
+    let recentActiveTab = document.querySelector('.tailored-tab-select-inner.active')
+    let nowActive = document.querySelector('[data-for-tab="' + tabIndex + '"]');
+    recentActiveTab.classList.remove('active');
+    document.querySelector('[data-tab="' + recentActiveTab.getAttribute('data-for-tab') + '"]').classList.remove('active');
+    nowActive.classList.add('active');
+    document.querySelector('[data-tab="' + nowActive.getAttribute('data-for-tab') + '"]').classList.add('active');
+}
+var play = new Timer();
+play.runTimer(1, 5)
